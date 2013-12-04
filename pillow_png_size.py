54aa8d95ee3cce38d9d343721d9f6447db481128
James Anderson <james.r.andreson@utah.edu>
2013-12-04 14:48:21 -0800

    Trying to figure out how to createlarge PNG files from numpy arrays.

---
+++ pillow_png_size.py
@@ -0,0 +1,52 @@
+'''
+Created on Dec 4, 2013
+
+@author: u0490822
+'''
+
+import numpy as np
+import scipy.misc as misc
+from PIL import Image
+
+def LargeImageSave():
+    '''Allocate an image with dimensions larger than 2^31'''
+
+    # for i in range(14, 17):
+        # dim = 1 << i
+        # dim -= 8
+    xdim = 48000
+    ydim = 32769
+    dtypestr = "uint8"
+    dtype = np.uint8
+
+    print "Saving this big png works for me"
+    outputname = '%dx%d_%s_test.png' % (xdim, ydim, dtypestr)
+
+#     a = np.zeros((xdim, ydim), dtype=dtype)
+#     # misc.imsave(outputname, a)
+#
+#     img = Image.fromarray(a, 'L')
+#     img.save(outputname)
+#
+#     del a
+#     del img
+
+    print "All done!"
+
+
+    print "Saving this big png does not work for me"
+    ydim = xdim
+    a = np.zeros((xdim, ydim), dtype=dtype)
+    # misc.imsave(outputname, a)
+
+    outputname = '%dx%d_%s_test.png' % (xdim, ydim, dtypestr)
+
+    img = Image.fromarray(a, 'L')
+    img.save(outputname)
+
+    print "All done!"
+
+
+if __name__ == '__main__':
+    LargeImageSave()
+    pass
