e71deb0abf06ab99055d4140e61d96e349e8fabc
James Anderson <james.r.andreson@utah.edu>
2013-12-04 14:51:15 -0800

    Uncommented some lines

--- pillow_png_size.py
+++ pillow_png_size.py
@@ -14,6 +14,7 @@ def LargeImageSave():
     # for i in range(14, 17):
         # dim = 1 << i
         # dim -= 8
+
     xdim = 48000
     ydim = 32769
     dtypestr = "uint8"
@@ -22,19 +23,21 @@ def LargeImageSave():
     print "Saving this big png works for me"
     outputname = '%dx%d_%s_test.png' % (xdim, ydim, dtypestr)
 
-#     a = np.zeros((xdim, ydim), dtype=dtype)
-#     # misc.imsave(outputname, a)
-#
-#     img = Image.fromarray(a, 'L')
-#     img.save(outputname)
-#
-#     del a
-#     del img
+    a = np.zeros((xdim, ydim), dtype=dtype)
+
+    # Ideally Scipy would work but it throws a different exception
+    # misc.imsave(outputname, a)
+
+    img = Image.fromarray(a, 'L')
+    img.save(outputname)
+
+    del a
+    del img
 
     print "All done!"
 
 
-    print "Saving this big png does not work for me"
+    print "Saving a slightly larger png does not work for me"
     ydim = xdim
     a = np.zeros((xdim, ydim), dtype=dtype)
     # misc.imsave(outputname, a)
